4174d45995ef9c1cafff8d154e260fb152bf8d88
cgtygrss <cgty5960@gmail.com>
2022-11-13 17:16:09 +0300

    Created new branch for testing

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 import numpy as np
 import pandas as pd
-import cv2 as openCV
+import cv2
 import matplotlib.pyplot as matplot
 import SimpleCV as simpleCV
+from scipy.spatial import distance as dist
+from imutils import perspective
+from imutils import contours
+
+
 
